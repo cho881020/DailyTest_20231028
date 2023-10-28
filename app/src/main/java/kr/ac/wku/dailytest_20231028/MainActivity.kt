e499547ea6be9b716c5f2350f3afde4f17e3c4ff
cho881020 <cho881020@gmail.com>
2023-10-28 17:45:36 +0900

    조건문 / == 비교 / && 논리연산 활용

--- app/src/main/java/kr/ac/wku/dailytest_20231028/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/dailytest_20231028/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.wku.dailytest_20231028
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.ac.wku.dailytest_20231028.databinding.ActivityMainBinding
 
@@ -18,7 +19,12 @@ class MainActivity : AppCompatActivity() {
             val inputEmail = binding.edtEmail.text.toString()
             val inputPassword = binding.edtPassword.text.toString()
 
-
+            if ( inputEmail == "admin@test.com" && inputPassword == "qwer" ) {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
 
         }
 
